ce525d54a2e0e2ec2329dc5262695b511e06e918
interns <195161749+L0nelys016@users.noreply.github.com>
2026-01-21 21:03:23 +0300

    add navigation

---
+++ UnityTauriGame/src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import "./Navigaion.css";
+
+export default function Navigaion() {
+    return (
+       <nav class="navigation">
+      <ul>
+        <li>
+          <a href="#/">Главная</a>
+        </li>
+      </ul>
+    </nav>
+    )
+}
