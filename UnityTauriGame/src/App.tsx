432a8b94cc1b40ec2d6fa2f7fe2ddbb7d93fa343
Artem Volkov <artemka.volkov2017@yandex.ru>
2026-01-26 09:03:45 +0300

    add main user page

--- UnityTauriGame/src/App.tsx
+++ UnityTauriGame/src/App.tsx
@@ -1,19 +1,24 @@
 import { createSignal } from "solid-js";
 import Auth from "./pages/Auth";
 import MainAdmin from "./pages/MainAdmin";
+import MainUser from "./pages/MainUser";
 import "./App.css";
 
 function App() {
-  const [currentPage, setCurrentPage] = createSignal<"auth" | "admin">("auth");
+  const [currentPage, setCurrentPage] = createSignal<"auth" | "admin" | "user">("auth");
 
   return (
     <div class="app">
       <main class="container">
-        {currentPage() === "auth" ? (
-          <Auth onLogin={() => setCurrentPage("admin")} />
-        ) : (
-          <MainAdmin />
+        {currentPage() === "auth" && (
+          <Auth 
+            // временно переключаем на user для теста
+            onLogin={() => setCurrentPage("user")} 
+          />
         )}
+
+        {currentPage() === "admin" && <MainAdmin />}
+        {currentPage() === "user" && <MainUser />}
       </main>
     </div>
   );
